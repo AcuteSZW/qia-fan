9ca09d6de040a1317775af943be095d3dad31157
AcuteSZW <53159377+AcuteSZW@users.noreply.github.com>
2021-01-18 16:47:14 +0800

    v1.0.01
    后端工程搭建

---
+++ qiafanhou/src/main/java/com/example/qiafan/QiafanApplication.java
@@ -0,0 +1,13 @@
+package com.example.qiafan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QiafanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QiafanApplication.class, args);
+    }
+
+}
---
+++ qiafanhou/src/test/java/com/example/qiafan/QiafanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qiafan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QiafanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
